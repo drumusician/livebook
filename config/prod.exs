1cb19be6331fbcc895e3612175da2c66af8348aa
Tjaco Oostdijk <info@drumusician.com>
2021-04-22 23:31:39 +0200

    port o.o.o.o

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,7 @@
 import Config
 
 # Default bind and port for production
-config :livebook, LivebookWeb.Endpoint, http: [ip: {127, 0, 0, 1}, port: 8080]
+config :livebook, LivebookWeb.Endpoint, http: [ip: {0, 0, 0, 0}, port: 8081]
 
 # Start log-level in notice by default to reduce output
 config :logger, level: :notice
